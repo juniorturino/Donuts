d2417903ac6a53b1977f3183f6f0b3cb10b300c8
Junior Turino <alvi.turino@gmail.com>
2025-10-02 20:03:31 -0300

    Codei a primeira seção do site

--- donuts.app/src/Components/Home.js
+++ donuts.app/src/Components/Home.js
@@ -1,11 +1,37 @@
 import Navbar from "./Navbar";
+import BannerBackground from "../Assets/home-banner-background.png";
+import BannerImage from "../Assets/home-banner-image.png";
+import { FiArrowRight } from "react-icons/fi";
 
 const Home = () => {
     return (
         <div className="home.container">
             <Navbar/>
+
+            <div className="home-banner-container">
+                <div className="home-bannerImage-container">
+                    <img src={BannerBackground} alt="Imagem de fundo do home container" />
+                </div>
+                <div className="home-text-section">
+                    <h1 className="primary-heading">Seu Donut Favorito Entregue Em Sua Casa</h1>
+                    <p className="primary-text">
+                        Diversos sabores deliciosos para adoçar seus melhores momentos. Peça já o seu e receba um mimo surpresa.
+                    </p>
+                    <button className="secondary-button">Faça seu pedido <FiArrowRight/> </button>
+
+                    <p className="primary-text">
+                        Ou venha comer conosco em um ambiente climatizado e aconchegante com várias opções para seu pedido.
+                    </p>
+                </div>
+                
+                <div className="home-image-section">
+                    <div className="home-primary-image">
+                        <img src={BannerImage} alt="Donuts"/>
+                    </div>
+                </div>
+            </div>
         </div>
     );
-}
+};
 
 export default Home;
